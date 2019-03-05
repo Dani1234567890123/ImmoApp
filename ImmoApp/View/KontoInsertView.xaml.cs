f376e58752d4a7e779f7e70a2551fee032542702
DanielaSchunke <danielaschunke@outlook.de>
2019-03-05 08:22:15 +0100

    ausgeschlossene Änderung zugefügt

---
+++ ImmoApp/View/KontoInsertView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static ImmoApp.View.ButtonMethoden;
+
+namespace ImmoApp.View
+{
+    /// <summary>
+    /// Interaktionslogik für KontoInsertView.xaml
+    /// </summary>
+    public partial class KontoInsertView : UserControl
+    {
+        public KontoInsertView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWeitKto_Click(object sender, RoutedEventArgs e)
+        {
+            Zwischenspeichern("Bankverbindung");
+        }
+
+        private void btnBankNeu_Click(object sender, RoutedEventArgs e)
+        {
+            Neu(new BankInsertView(), "Neues Kreditinsitut");
+        }
+
+
+        private void btnspeichern_Click(object sender, RoutedEventArgs e)
+        {
+            Speichern((Window)this.Parent);
+        }
+
+        private void btnAbbruch_Click(object sender, RoutedEventArgs e)
+        {
+            Abbrechen((Window)this.Parent);
+        }
+    }
+}
